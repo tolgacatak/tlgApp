06cb9c4ce790f0cf8fafcabf01a1dc0dbf4b112a
tqyl <106453124+tqyl@users.noreply.github.com>
2023-12-01 18:48:32 +0300

    postlist added

--- src/main/java/com/example/tlgaskapp/DTO/PostDTO.java
+++ src/main/java/com/example/tlgaskapp/DTO/PostDTO.java
@@ -2,6 +2,7 @@ package com.example.tlgaskapp.DTO;
 //Post işlemi yapıldığında user bilgilerini de elde etmek için
 import com.example.tlgaskapp.entities.PostEntity;
 import lombok.Data;
+import java.util.List;
 
 @Data
 public class PostDTO {
@@ -10,7 +11,7 @@ public class PostDTO {
     String userName;
     String text;
     String title;
-
+    List<PostEntity> postList;
     public PostDTO(PostEntity entity){ //Constructor based mapper
         this.id = entity.getId();
         this.userId = entity.getUser().getId();
